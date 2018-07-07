af8c76008eaad635894d94e27b786524d0367a37
Andrey-rudnik <andrey-rudnik199122sony@rambler.ru>
2018-07-07 18:15:31 +0300

    start mob

--- src/components/Header/Sidebar.vue
+++ src/components/Header/Sidebar.vue
@@ -1,8 +1,20 @@
 <template lang="html">
   <aside class="sidebar" v-show="show" v-bind:class="{slide: slide}">
-    <ul>
-      <router-link :to="{ name: 'Films', params: {} }" tag="li">Фильмы</router-link>
-      <router-link :to="{ name: 'Series', params: {} }" tag="li">Сериалы</router-link>
+    <ul> 
+      <router-link :to="{ name: 'Films', params: {} }" tag="li" class="iconn1">
+        <img class="icon" src="@/assets/icon1-mobile360.png" alt="logo">Фильмы</router-link>
+        <ul>
+          <router-link :to="{ name: 'subcategory1', params: {} }" tag="li" class="subcategory1">Подкатегория 1</router-link>
+          <router-link :to="{ name: 'subcategory2', params: {} }" tag="li" class="subcategory1">Подкатегория 2</router-link>
+          <router-link :to="{ name: 'subcategory3', params: {} }" tag="li" class="subcategory1">Подкатегория 3</router-link>
+          <router-link :to="{ name: 'subcategory4', params: {} }" tag="li" class="subcategory1">Подкатегория 4</router-link>
+        </ul>
+      
+      <router-link :to="{ name: 'Series', params: {} }" tag="li"  class="iconn2">
+        <img class="icon" src="@/assets/icon2-mobile360.png" alt="logo">Сериалы</router-link>
+      
+      <router-link :to="{ name: 'Favorites', params: {} }" tag="li"  class="iconn3">
+        <img class="icon" src="@/assets/icon3-mobile360.png" alt="logo">Избранное</router-link>
     </ul>
   </aside>
 </template>
@@ -23,17 +35,42 @@ export default {
 <style lang="css">
   .sidebar {
     background-color: #fff;
-    width: 300px;
-    position: fixed;
+    width: 304px;
+    height: 100%;
+    position: absolute;  /*fixed */
     top: 56px;
     left: 0;
     bottom: 0;
     z-index: 1;
     display: flex;
-    justify-content: center;
+    /* justify-content: center; */
     align-items: center;
     box-shadow:  0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
     opacity: .9;
+  }
+
+  .icon{
+    padding-right: 34px;
+  }
+  .iconn1{
+    margin-top: -290px;
+  }
+
+  .iconn2{
+    padding: 27px 0 27px 0;
+  }
+
+  .iconn1, .iconn2, .iconn3{
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 14px;
+    color: #372c48;
+  }
+
+  .subcategory1{
+    padding-left: 60px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 12px;
+    color: #372c48;
   }
 
   .slide-transition {
@@ -42,5 +79,23 @@ export default {
 
   .slide-enter, .slide-leave {
     left: -100%;
+  }
+
+  ul{
+    margin-left: 16px;
+  }
+
+  li{
+    padding-top: 11px;
+  }
+
+  li:hover{
+    color: #77c1bb;
+  }
+
+  @media only screen and (min-device-width : 360px) and (max-device-width : 480px) {
+    .sidebar{
+      
+    }
   }
 </style>
